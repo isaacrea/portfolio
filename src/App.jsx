7e3eef027605dad26bbd36ba809d3d43cab8d498
Isaac Rea <117219102+isaacrea@users.noreply.github.com>
2024-12-16 17:56:21 +0000

    Updated personal information.

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Isaac Rea",
+  title: "Software Engineer & Cybersecurity Enthusiast",
+  email: "isaacrea10@gmail.com",
+  gitHub: "isaacrea",
+  instagram: "isaacrea1",
+  linkedIn: "isaacrea",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "irea_sw",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
