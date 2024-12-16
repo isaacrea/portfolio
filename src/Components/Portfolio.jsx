6b7b11733b292c24fb87080fd7dd55208e834a24
Isaac Rea <117219102+isaacrea@users.noreply.github.com>
2024-12-16 20:09:20 +0000

    Updated styles

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,39 +1,13 @@
-/**
- * Portfolio component
- *
- * Highlights some of  your creations. These can be designs, websites,
- * open source contributions, articles you've written and more.
- *
- * This is a great area for you to to continually add to and refine
- * as you continue to learn and create.
- */
-
 import React from "react";
-
-/**
- * Desk image
- *
- * Below is a sample desk image. Feel free to update this to an image of your choice,
- * updating below imageAltText to string that represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a photo you
- * freely use on your site.
- */
 import image from "../images/design-desk.jpeg";
 
 const imageAltText = "desktop with books and laptop";
 
-/**
- * Project list
- *
- * An array of objects that will be used to display for your project
- * links section. Below is a sample, update to reflect links you'd like to highlight.
- */
 const projectList = [
   {
     title: "Sonifi: Accessible Data Visualization with Sound",
     description:
-      "Collaborated with a teama to design and develop a web app that creates accessible, sonified graphs for both sighted and visually impaired users. Followed WCAG standards and utilized Agile methodology.",
+      "Collaborated with a team to design and develop a web app that creates accessible, sonified graphs for both sighted and visually impaired users. Followed WCAG standards and utilized Agile methodology.",
     url: "https://github.com/DillonEdington/SonifyGraphs",
   },
   {
@@ -64,6 +38,7 @@ const Portfolio = () => {
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
             src={image}
+            className="slideInLeft"
             style={{ height: "90%", width: "100%", objectFit: "cover" }}
             alt={imageAltText}
           />
